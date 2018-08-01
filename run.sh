f306353cff5c1fd45be2583273359c4afd312be3
abdullah mamun <awsdevopro@gmail.com>
2018-08-01 17:18:36 +0600

    add bash

--- run.sh
+++ run.sh
@@ -4,13 +4,11 @@ ImageTestA = $(docker inspect -f '{{.State.Running}}' docker-image-A);
 ImageTestB = $(docker inspect -f '{{.State.Running}}' docker-image-B);
 ImageRemoveA = $(docker rm -f docker-image-A);
 ImageRemoveB = $(docker rm -f docker-image-B);
-ImageRunA = $(docker run -d --name docker-image-A awsdevopro/php-app:1.1.1);
-ImageRunB = $(docker run -d --name docker-image-B awsdevopro/php-app:1.1.1);
+ImageRunA = $(docker run -d -p 8888:80 --name docker-image-A awsdevopro/php-app:1.1.1);
+ImageRunB = $(docker run -d -p 8383:80 --name docker-image-B awsdevopro/php-app:1.1.1);
 #CheckPort = $(docker inspect docker-image-test | grep HostPort);
-if [ $ImageTestA ]; then
+if [ $ImageTestA = 'true' ]; then
      $ImageRunB && $ImageRemoveA;
-elif [ $ImageTestB ]; then
-                  $ImageRunA && $ImageRemoveB;
 else
 $ImageRunA
 fi
