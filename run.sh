8d466211559d2c042d4a6cf4efac0d13da10f795
abdullah mamun <awsdevopro@gmail.com>
2018-08-01 17:37:05 +0600

    add bash

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-imagetesta = $(docker inspect -f '{{.State.Running}}' docker-image-A);
-imagetestb = $(docker inspect -f '{{.State.Running}}' docker-image-B);
-imageremovea = $(docker rm -f docker-image-A);
-imageremoveb = $(docker rm -f docker-image-B);
-imageruna = $(docker run -d -p 8888:80 --name docker-image-A awsdevopro/php-app:1.1.1);
-imagerunb = $(docker run -d -p 8383:80 --name docker-image-B awsdevopro/php-app:1.1.1);
+ImageTestA = $(docker inspect -f '{{.State.Running}}' docker-image-A);
+ImageTestB = $(docker inspect -f '{{.State.Running}}' docker-image-B);
+ImageRemoveA = $(docker rm -f docker-image-A);
+ImageRemoveB = $(docker rm -f docker-image-B);
+ImageRunA = $(docker run -d -p 8888:80 --name docker-image-A awsdevopro/php-app:1.1.1);
+ImageRunB = $(docker run -d -p 8383:80 --name docker-image-B awsdevopro/php-app:1.1.1);
 #CheckPort = $(docker inspect docker-image-test | grep HostPort);
-if [ $imagetesta = "true" ]
+if [ '${ImageTestA}' = 'yes' ]
 then
-   $imagerunb && $imageremovea
+  ${ImageRunB} && ${ImageRemoveA}
 else
-   $imageruna
+${ImageRunA}
 fi
 
 
