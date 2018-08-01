e60192f3cf3c009659430b9c3e8ee8557bfdc16b
Mamun <abdullah.mamun@itconquest.com>
2018-08-01 06:05:24 +0600

    add script

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+if [ $(docker inspect -f '{{.State.Running}}' docker-image-test) = "true" ]; then 
+     
+      docker rm -f docker-image-test && docker run -p 8888:80 -d --name docker-image-test awsdevopro/php-app:1.1.0;
+ 
+                else 
+                       docker run -p 8888:80 -d --name docker-image-test awsdevopro/php-app:1.1.0; 
+
+fi
+
