4322f751a7ecd6a530f0eb6f0e3e235e71abc6d0
abdullah mamun <awsdevopro@gmail.com>
2018-08-01 15:42:58 +0600

    variable declare in run

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-ImageTest = $(docker inspect -f '{{.State.Running}}' docker-image-test)
-ImageRemove = docker rm -f docker-image-test
-ImageRun = docker run -p 8888:80 -d --name docker-image-test awsdevopro/php-app:1.1.1
+ImageTest = $(docker inspect -f '{{.State.Running}}' docker-image-test);
+ImageRemove = $(docker rm -f docker-image-test);
+ImageRun = $(docker run -p 8888:80 -d --name docker-image-test awsdevopro/php-app:1.1.1);
 if [ $ImageTest = "true" ]; then
      $ImageRemove && $ImageRun;
              else
                   $ImageRun;
 fi
-
