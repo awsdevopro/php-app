14a7c8ef341770f07635cc8312f224e0f6b35b90
abdullah mamun <awsdevopro@gmail.com>
2018-08-01 16:46:40 +0600

    add bash

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ ImageRemoveB = $(docker rm -f -p 8383:80 docker-image-test);
 ImageRunA = $(docker run -p 8888:80 -d --name docker-image-test awsdevopro/php-app:1.1.1);
 ImageRunB = $(docker run -p 8383:80 -d --name docker-image-test awsdevopro/php-app:1.1.1);
 CheckPort = $(docker inspect docker-image-test | grep HostPort);
-if [ $CheckPort = "8888" ]; then
+if [ $CheckPort == "8888" ]; then
      $ImageRunB && $ImageRemoveA;
 else
                   $ImageRunA && $ImageRemoveB;
